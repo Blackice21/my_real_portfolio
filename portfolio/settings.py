ac6203b9ae684f5bc2ea490a0a9886ed920c3ff9
Marcus Morrow <marcusmorrow319@gmail.com>
2021-04-28 15:41:20 -0500

    another one

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0hn08tcx==$s8pc#2j+*ea^y9-k21@zwgq&_zjb!ddqo2ppd@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
