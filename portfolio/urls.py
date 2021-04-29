cc15de838d5a344b8e090c5a0bb28e54e3dcb66a
Marcus Morrow <marcusmorrow319@gmail.com>
2021-04-29 16:38:14 -0500

    added static config to urls.py in project folder

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,4 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
