d95760bc67c7b30ebed9ffce7ee438f4c93aece9
Marcus Morrow <marcusmorrow319@gmail.com>
2021-04-30 14:46:40 -0500

    fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'portfolio/statics/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'portfolio/static/')]
 
 
 # Default primary key field type
